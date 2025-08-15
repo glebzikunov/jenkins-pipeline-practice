fbb9f7910f45667a410d4b478ec843c4ff21bb8d
Gleb Zikunov <zikunovga@gmail.com>
2025-08-15 15:02:10 +0300

    feat: add allure steps

--- test/specs/jenkinsTest.js
+++ test/specs/jenkinsTest.js
@@ -1,20 +1,30 @@
-import { assert } from "chai"
-import Browser from "../../framework/browser/Browser.js"
-import { mainConfig } from "../../framework/configs/main.wdio.conf.js"
-import MainPage from "../pageObjects/MainPage.js"
-import DynamicControlsPage from "../pageObjects/DynamicControlsPage.js"
-import data from "../testData/data.json" assert { type: "json" }
+import { assert } from "chai";
+import Browser from "../../framework/browser/Browser.js";
+import { mainConfig } from "../../framework/configs/main.wdio.conf.js";
+import MainPage from "../pageObjects/MainPage.js";
+import DynamicControlsPage from "../pageObjects/DynamicControlsPage.js";
+import data from "../testData/data.json" assert { type: "json" };
+import AllureReporter from "@wdio/allure-reporter";
 
 describe("Jenkins parameters test", function () {
   it("Checks that string passed as a parameter from Jenkins matches with a string from test data", async function () {
-    await Browser.openUrl(mainConfig.baseUrl)
-    await MainPage.clickNavigationLink("Dynamic Controls")
-    console.log(mainConfig.inputString)
+    AllureReporter.addStep("Open base heroku url");
+    await Browser.openUrl(mainConfig.baseUrl);
 
-    await DynamicControlsPage.clickEnableButton()
-    assert.isTrue(await DynamicControlsPage.isInputFieldEnabled(), "Input is not enabled")
+    AllureReporter.addStep("Click on dynamic controls nav link");
+    await MainPage.clickNavigationLink("Dynamic Controls");
+    console.log(mainConfig.inputString);
 
-    await DynamicControlsPage.inputTextIntoInputField(mainConfig.inputString)
-    assert.strictEqual(await DynamicControlsPage.getInputFieldValue(), data.inputString, "Strings are not equal")
-  })
-})
+    AllureReporter.addStep("Click enable button");
+    await DynamicControlsPage.clickEnableButton();
+
+    AllureReporter.addStep("Assert if input is enabled");
+    assert.isTrue(await DynamicControlsPage.isInputFieldEnabled(), "Input is not enabled");
+
+    AllureReporter.addStep("Input string that passed from Jenkins");
+    await DynamicControlsPage.inputTextIntoInputField(mainConfig.inputString);
+
+    AllureReporter.addStep("Assert that strings are equal");
+    assert.strictEqual(await DynamicControlsPage.getInputFieldValue(), data.inputString, "Strings are not equal");
+  });
+});
